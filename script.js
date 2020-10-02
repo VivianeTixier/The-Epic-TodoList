ba3677ab8b74cb0b7a7b7e492ceec638c6b5eb51
Viviane <viviane.tixier.simplon@gmail.com>
2020-10-02 20:07:37 +0200

    ajout du script permettant d'afficher une liste s'il y a des taches ou un message s'il n'y en a pas

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("HANLALA");
+//On cible le ul contenant la liste de tâche
+let $ulList = document.querySelector('#list');
+
+//On crée un tableau de tâches
+let taskArray = ["blabla", "blibli"];
+
+//On lui met un html par défaut s'il n'y a aucune tâche dans le tableau list
+//Et on affiche les tâches s'il y en a une
+if (taskArray.length > 0) {
+    for (task of taskArray) {
+        //On crée le li
+        let li = document.createElement("li");
+        //On insère la donnée dedans
+        li.innerHTML= `${task}`;
+        //On ajoute le li au ul
+        $ulList.appendChild(li);
+    }
+} else {
+    $ulList.innerHTML = "Il n'y a aucune tâche pour le moment."
+}
